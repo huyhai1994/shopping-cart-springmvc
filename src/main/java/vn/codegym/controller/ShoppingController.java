cc1bc121befc8780cdf2102673146b2d46669755
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-25 08:19:52 +0700

    lenovo_huyhai1994 ShoppingCart Controller

---
+++ src/main/java/vn/codegym/controller/ShoppingController.java
@@ -0,0 +1,24 @@
+package vn.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import vn.codegym.model.Cart;
+
+@Controller
+public class ShoppingController {
+
+    @ModelAttribute("cart")
+    public Cart setupCart() {
+        return new Cart();
+    }
+
+    @GetMapping("/shopping-cart")
+    public ModelAndView showCart(@SessionAttribute("cart") Cart cart) {
+        ModelAndView modelAndView = new ModelAndView("/cart");
+        modelAndView.addObject("cart", cart);
+        return modelAndView;
+    }
+}
