7d4983ca0dfb0b56b5ee61c58ab01db5bdc89018
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-25 08:32:48 +0700

    lenovo_huyhai1994 fixed Repository created Bean -> add Annotation JpaRepo

--- src/main/java/vn/codegym/configuration/ApplicationConfig.java
+++ src/main/java/vn/codegym/configuration/ApplicationConfig.java
@@ -9,12 +9,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -28,8 +31,11 @@ import javax.persistence.EntityManagerFactory;
 import java.util.Properties;
 
 @Configuration
-@ComponentScan("vn.codegym")
 @EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = "vn.codegym")
+@EnableJpaRepositories("vn.codegym.repository")
+@EnableSpringDataWebSupport
 public class ApplicationConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
     @Autowired
     private Environment env;
@@ -107,6 +113,7 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
         return properties;
     }
 }
